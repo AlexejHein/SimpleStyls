2d8defc9664a59d785efc2751db7faa8bb485a43
Alexej Hein <alexej.hein@aol.de>
2024-03-17 16:48:22 +0100

    commit

--- main.js
+++ main.js
@@ -2,3 +2,25 @@
 function toggleMenu() {
     document.getElementById('sidebar').classList.toggle('active');
 }
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showNextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % totalSlides;
+    slides[currentSlide].classList.add('active');
+}
+
+// Automatischer Wechsel alle 2 Sekunden
+setInterval(showNextSlide, 4000);
+
+// Manuelle Steuerung (Optional)
+document.querySelector('.next').addEventListener('click', showNextSlide);
+
+document.querySelector('.prev').addEventListener('click', () => {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    slides[currentSlide].classList.add('active');
+});
+
